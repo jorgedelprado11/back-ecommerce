6605efcbc0df176200ce85a920b58d0a878ff0fc
jorgedelprado11 <jorgedelprado11@gmail.com>
2023-12-08 19:58:51 -0300

    volviendo a la normalidad

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 
 import uvicorn
 from os import getenv
+from fastapi import FastAPI
+from routers import products
 
+
+app = FastAPI()
+
+# routers
+app.include_router(products.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
 
 if __name__ == "__main__":
     port = int(getenv("PORT", 8000))
