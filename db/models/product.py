8f5acf793c8e5866bc4fd577b12eff10d2e24b52
jorgedelprado11 <jorgedelprado11@gmail.com>
2023-12-08 14:07:10 -0300

    agregando vercel

--- db/models/product.py
+++ db/models/product.py
@@ -1,13 +1,13 @@
 from pydantic import BaseModel
-
+from typing import Optional
 # Product Model
 
 
 class Product(BaseModel):
-    id: str | None = None
+    id: Optional[str]
     nombre: str
     categoria: str
     precio: int | float
     stock: int
     imagenes: list
-    caracteristicas: dict | None = None
+    caracteristicas: Optional[dict]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from routers import products
 from fastapi.staticfiles import StaticFiles
+import uvicorn
 
 app = FastAPI()
 
@@ -11,3 +12,7 @@ app.include_router(products.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
