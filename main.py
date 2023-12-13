6da793b71f7c1a847bdce8bd024ebf0474569bf9
jorgedelprado11 <jorgedelprado11@gmail.com>
2023-12-13 19:41:11 -0300

    agregando los allow esos

--- main.py
+++ main.py
@@ -2,10 +2,19 @@
 import uvicorn
 from os import getenv
 from fastapi import FastAPI
-from routers import products,users
-
+from routers import products, users
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+origin = [
+    # 'http://localhost:3000'
+    '*'
+]
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origin,
+                   allow_credentials=True,
+                   allow_methods=['*'],
+                   allow_headers=['*'])
 
 # routers
 app.include_router(products.router)
@@ -15,5 +24,3 @@ app.include_router(users.router)
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
